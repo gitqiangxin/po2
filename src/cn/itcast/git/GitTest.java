8147e266c1dc17490297ae60e4291b1a7fcd01c0
王彪 <m15936571711_2@163.com>
2018-12-17 20:13:25 +0800

    GitTest

---
+++ src/cn/itcast/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("fafsdggsdfs");
+        System.out.println("fafsdggsdfs");
+        System.out.println("fafsdggsdfs");
+        System.out.println("fafsdggsdfs");
+        System.out.println("fafsdggsdfs");
+        System.out.println("fafsdggsdfs");
+    }
+}
